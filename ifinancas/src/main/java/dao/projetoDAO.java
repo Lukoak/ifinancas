6f302e48d2780365e0425dd01060efa73841982b
Lucas <lucas_carvalho777@hotmail.com>
2026-08-09 19:34:15 -0300

    d

--- ifinancas/src/main/java/dao/projetoDAO.java
+++ ifinancas/src/main/java/dao/projetoDAO.java
@@ -1,4 +1,5 @@
 package dao;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -8,26 +9,23 @@ import util.conexao;
 
 public class projetoDAO {
 	
-	public boolean insereprojeto(projeto coordenador_id, projeto titulo, projeto status_projeto, projeto descricao)
-	{
-		String inserir_projeto = "INSERT INTO " +
-								"projeto(coordenador_id, titulo, " +
-								"status_projeto, descricao " +
-								"VALUES (?,?,?,?)";
+	public boolean insereprojeto(projeto p) {
+		// Parêntese fechado corretamente na sintaxe do SQL.
+		String inserir_projeto = "INSERT INTO projeto (coordenador_id, titulo, status_projeto, descricao) VALUES (?, ?, ?, ?)";
 		
 		try (Connection conn = conexao.getConexao();
-				PreparedStatement stmt = conn.prepareStatement(inserir_projeto))
-		{
-			stmt.setInt(1, coordenador_id.getcoordenador_id());
-			stmt.setString(2, titulo.gettitulo());
-			stmt.setString(3, status_projeto.getStatus_projeto());
-			stmt.setString(4, descricao.getDescricao());
+			 PreparedStatement stmt = conn.prepareStatement(inserir_projeto)) {
+			
+			stmt.setInt(1, p.getcoordenador_id());
+			stmt.setString(2, p.gettitulo());
+			stmt.setString(3, p.getStatus_projeto());
+			stmt.setString(4, p.getDescricao());
 			
 			return stmt.executeUpdate() > 0;
-		}catch (SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
 		}		
 	}
-
 }
