63ac4fbe181bec5c650eb007b16c943f692f9530
Lucas <lucas_carvalho777@hotmail.com>
2026-08-17 00:15:16 -0300

    ddw

--- src/main/java/util/conexao.java
+++ src/main/java/util/conexao.java
@@ -1,4 +1,4 @@
-package util;
+/*package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,6 +13,34 @@ public class conexao {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver JDBC do MySQL não encontrado!", e);
+        }
+    }
+}
+
+*/
+
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexao {
+
+    private static final String HOST = "ifinance-ifba.a.aivencloud.com";
+    private static final String PORT = "21900";
+    private static final String DATABASE = "defaultdb";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE 
+            + "?useSSL=true&requireSSL=true&verifyServerCertificate=false&allowPublicKeyRetrieval=true";
+
+    public static Connection getConexao() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
             throw new SQLException("Driver JDBC do MySQL não encontrado!", e);
         }
